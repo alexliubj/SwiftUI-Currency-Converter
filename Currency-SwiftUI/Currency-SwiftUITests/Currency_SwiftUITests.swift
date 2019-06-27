9ff6053e922d54b78d14a0fa935b3af3abe38443
Alex Liu <alexliubo@gmail.com>
2019-06-27 01:36:21 -0400

    First init commit

---
+++ Currency-SwiftUI/Currency-SwiftUITests/Currency_SwiftUITests.swift
@@ -0,0 +1,34 @@
+//
+//  Currency_SwiftUITests.swift
+//  Currency-SwiftUITests
+//
+//  Created by Alex Liu on 2019-06-27.
+//  Copyright © 2019 Alex Liu. All rights reserved.
+//
+
+import XCTest
+@testable import Currency_SwiftUI
+
+class Currency_SwiftUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
